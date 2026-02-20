74b9b48a3e6fe759a4a443fc7333c5c098062466
AmardeepGunaki0607 <168105738+AmardeepGunaki0607@users.noreply.github.com>
2026-02-20 14:25:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (49.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp=[-1]*len(nums)
-        return self.rec(0,nums,dp)
+    #     dp=[-1]*len(nums)
+    #     return self.rec(0,nums,dp)
 
-    def rec(self,i,nums,dp):
-        if i>=len(nums):
-            return 0
-        if dp[i]!=-1:
-            return dp[i]
-        take=nums[i]+self.rec(i+2,nums,dp)
-        not_take=self.rec(i+1,nums,dp) 
-        dp[i]=max(take,not_take)
-        return dp[i]
+    # def rec(self,i,nums,dp):
+    #     if i>=len(nums):
+    #         return 0
+    #     if dp[i]!=-1:
+    #         return dp[i]
+    #     take=nums[i]+self.rec(i+2,nums,dp)
+    #     not_take=self.rec(i+1,nums,dp) 
+    #     dp[i]=max(take,not_take)
+    #     return dp[i]
+        if len(nums)==1:
+            return nums[0]
+        dp=[0]*len(nums)
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            dp[i]=max(dp[i-1],nums[i]+dp[i-2])
+        return dp[-1]
